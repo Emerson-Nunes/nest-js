75c67d35155cdd58170aee7fd8c249b7fbc6cfdb
Emerson-Nunes <emerson_nunes2@hotmail.com>
2026-03-24 11:23:18 -0300

    Alteração no gitignore para ignorar outros .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { APP_GUARD } from '@nestjs/core';
         ttl: 60000, //Está em milissegundos
         limit: 10 //Limite de requisições
       }]
-    })],
+    }),
+  ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthService } from "./auth.service";
 @Module({
     imports: [
         JwtModule.register({
-            secret: JWT_SECRET
+            secret: process.env.JWT_SECRET,
         }),
         forwardRef(() => UserModule),
         PrismaModule
